3995c93ac89bc94918486bc048a4c676085982fe
estenssoros <seb.estenssoro@gmail.com>
2019-10-27 16:09:26 -0600

    methods to create and update

--- models/dagrun.go
+++ models/dagrun.go
@@ -3,13 +3,27 @@ package models
 import (
 	"time"
 
+	"github.com/estenssoros/goflow/db"
 	"github.com/estenssoros/goflow/state"
 )
 
+// DagRun describes an instance of a Dag. It can be created by the scheduler or by an external trigger
 type DagRun struct {
-	ID        int
-	DagID     string
-	StartDate time.Time
-	EndDate   time.Time
-	State     state.State
+	ID            int
+	DagID         string
+	ExecutionDate time.Time
+	State         state.State
+	StartDate     time.Time
+	EndDate       time.Time
+}
+
+func (d *DagRun) Create() error {
+	conn := db.Connection
+	return conn.Create(d).Error
+}
+
+func (d *DagRun) UpdateState(s state.State) error {
+	conn := db.Connection
+	conn.Model(d).Update("state", s)
+	return conn.Error
 }
