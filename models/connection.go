ac343fd15fbbe186cc3b4f784c42f773f0364671
estenssoros <seb.estenssoro@gmail.com>
2019-10-27 16:08:46 -0600

    updated to match airflow more

--- models/connection.go
+++ models/connection.go
@@ -1,16 +1,17 @@
 package models
 
-import (
-	"github.com/estenssoros/goflow/connection"
-)
-
+// Connection placeholder to store information about different database instances
+// connection information. The idea here is that scripts use references to
+// database instances (conn_id) instead of hard coding hostname, logins and
+// passwords when using operators or hooks.
 type Connection struct {
-	Base
-	ConnName string
-	ConnType connection.Type
+	ID       int
+	ConnID   string
+	ConnType string
 	Host     string
 	Schema   string
 	Login    string
 	Password string
 	Port     int
+	Extra    string
 }
