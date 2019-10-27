63b97b7d1e30a21026d719fe18ea31a91d4ffbde
estenssoros <seb.estenssoro@gmail.com>
2019-10-27 16:09:35 -0600

    fields that we need

--- models/dag.go
+++ models/dag.go
@@ -4,15 +4,14 @@ import (
 	"time"
 )
 
-type Dag struct {
-	ID                 string
-	IsPausedAtCreation bool
-	IsPaused           bool
-	IsSubDag           bool
-	IsActive           bool
-	LastSchedulerRun   time.Time
-	Owners             string
-	Description        string
-	DefaultView        string
-	ScheduleInterval   string
+// DAG (directed acyclic graph) a collection of tasks with directional
+// dependencies
+type DAG struct {
+	ID               string `gorm:"PRIMARY_KEY"`
+	IsPaused         bool
+	LastSchedulerRun time.Time
+	Owners           string
+	Description      string
+	DefaultView      string
+	ScheduleInterval string
 }
