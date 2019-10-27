60d9c4f043a5899b3db87a91f642918cfc6efadd
estenssoros <seb.estenssoro@gmail.com>
2019-10-27 11:39:56 -0600

    doc strings

--- webserver.go
+++ webserver.go
@@ -14,19 +14,24 @@ import (
 	"github.com/pkg/errors"
 )
 
+// Webserver handles the webserver
 type Webserver struct {
 	Dags map[string]*DAG
 }
 
+// NewWebserver creates a new webserver from a dag map
 func NewWebserver(dags map[string]*DAG) *Webserver {
 	return &Webserver{dags}
 }
+
+// Routes applies routes to echo
 func (w *Webserver) Routes(e *echo.Echo) {
 	e.GET("/", func(e echo.Context) error {
 		return e.Render(http.StatusOK, "index.html", nil)
 	})
 }
 
+// Serve serves the web endopoints
 func (w *Webserver) Serve() error {
 	e := echo.New()
 	e.Use(middleware.Logger())
@@ -45,10 +50,12 @@ func (w *Webserver) Serve() error {
 	return nil
 }
 
+// Template holdes templates
 type Template struct {
 	templates *lib.FileGroup
 }
 
+// Render implements the echo render function
 func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
 	ts := t.templates.String(name)
 	if ts == "" {
