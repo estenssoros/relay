3f1a32a320d4424ddf9f5026218df9ced8c85bb6
estenssoros <seb.estenssoro@gmail.com>
2019-10-30 08:22:40 -0700

    additional states

--- state/state.go
+++ state/state.go
@@ -1,12 +1,16 @@
 package state
 
 var (
-	Pending        State = "pending"
-	Queued         State = "queued"
 	Success        State = "success"
-	Failed         State = "failed"
-	UpstreamFailed State = "upstream-failed"
 	Running        State = "running"
+	Failed         State = "failed"
+	Skipped        State = "skipped"
+	Rescheduled    State = "rescheduleud"
+	Retry          State = "retry"
+	Queued         State = "queued"
+	Pending        State = "pending"
+	UpstreamFailed State = "upstream-failed"
+	None           State = "none"
 )
 
 type State string
